f8df2ee8e85aefacd11a21d124b497d0260b751c
Radis Valiullin <radisrower@gmail.com>
2024-03-13 16:26:03 -0700

    ccode6ed: prtvi: ex1: go emplem, v1

--- ccode6ed/prtvi/ex1/v1/main.go
+++ ccode6ed/prtvi/ex1/v1/main.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("main")
+
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	sum, prod := foo(a)
+	fmt.Printf("sum and prod: %v, %v\n", sum, prod)
+}
+
+func foo(arr []int) (sum, prod int) {
+	sum = 0
+	prod = 1
+	for _, v := range arr {
+		sum += v
+	}
+	for _, v := range arr {
+		prod *= v
+	}
+	return sum, prod
 }
