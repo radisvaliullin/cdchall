504b38c42db13d440a0500f0fb4d21bdafcb567c
Radis Valiullin <radisrower@gmail.com>
2024-03-13 16:26:03 -0700

    ccode6ed: prtvi: ex4: go emplem, v1

---
+++ ccode6ed/prtvi/ex4/v1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("main")
+
+	arrA := []int{43, 73, 1, 2, 3, 4}
+	arrB := []int{44, 55, 66}
+	printPair(arrA, arrB)
+}
+
+// O(a*b)
+func printPair(arrA, arrB []int) {
+	for i := 0; i < len(arrA); i++ {
+		for j := 0; j < len(arrB); j++ {
+			fmt.Printf("pair: %v - %v\n", arrA[i], arrB[j])
+		}
+	}
+}
