d4adc71db49e5b65dd0e71def585e0474071e120
Radis Valiullin <radisrower@gmail.com>
2024-03-19 02:26:57 -0700

    ccode6ed: prtix: ch1: qst: q5: v1 go ver, upd v1.1

--- ccode6ed/prtix/ch1/qst/q5/v1/main.go
+++ ccode6ed/prtix/ch1/qst/q5/v1/main.go
@@ -14,26 +14,36 @@ func main() {
 	str1 := "qwerty日の出asdf"
 	str2 := "qwerty日のgasdf"
 	fmt.Println(isOneEditAway(str1, str2))
+	fmt.Println(isOneEditAwayV2(str1, str2))
+	fmt.Println("")
 
 	// one insert
 	str1 = "qwerty日の出asdf"
 	str2 = "qwerty日の出asdf1"
 	fmt.Println(isOneEditAway(str1, str2))
+	fmt.Println(isOneEditAwayV2(str1, str2))
+	fmt.Println("")
 
 	// two insert
 	str1 = "qwerty日の出asdf"
 	str2 = "qwerty日の出asdf12"
 	fmt.Println(isOneEditAway(str1, str2))
+	fmt.Println(isOneEditAwayV2(str1, str2))
+	fmt.Println("")
 
 	// one remove
 	str1 = "qwerty日の出asdf"
 	str2 = "qwerty日の出asd"
 	fmt.Println(isOneEditAway(str1, str2))
+	fmt.Println(isOneEditAwayV2(str1, str2))
+	fmt.Println("")
 
 	// two remove
 	str1 = "qwerty日の出asdf"
-	str2 = "qwerty日の出asd"
+	str2 = "qwerty日の出as"
 	fmt.Println(isOneEditAway(str1, str2))
+	fmt.Println(isOneEditAwayV2(str1, str2))
+	fmt.Println("")
 }
 
 // cpu - O(N), where N - len(s1)+len(s2), we find len by iterating each string
@@ -104,12 +114,61 @@ func isOneEditInsertFirstStr(s1, s2 string) bool {
 			if idx1 != idx2 {
 				return false
 			}
-			idx2++
+			idx2 += len2
 		} else {
 			idx1 += len
 			idx2 += len2
 		}
 	}
 
+	return true
+}
+
+// version 2 (less code, less clear)
+func isOneEditAwayV2(s1, s2 string) bool {
+	s1len := utf8.RuneCountInString(s1)
+	s2len := utf8.RuneCountInString(s2)
+	diffLen := (s1len - s2len)
+	if diffLen < 0 {
+		diffLen *= -1
+	}
+	if diffLen > 1 {
+		return false
+	}
+	if s1len > s2len {
+		// copy string in Go do not copy underlying array just string header (pointer to array and len)
+		// so it's cheap
+		s1, s2 = s2, s1
+	}
+
+	// flag for detect first diff
+	isDiff := false
+	idx1 := 0
+	idx2 := 0
+	for idx1 < len(s1) && idx2 < len(s2) {
+		r, len := utf8.DecodeRuneInString(s1[idx1:])
+		// if not utf8 return false
+		if r == utf8.RuneError {
+			return false
+		}
+		r2, len2 := utf8.DecodeRuneInString(s2[idx2:])
+		// if not utf8 return false
+		if r2 == utf8.RuneError {
+			return false
+		}
+		if len != len2 || r != r2 {
+			// we get second diff char
+			if isDiff {
+				return false
+			}
+			isDiff = true
+			if s1len == s2len {
+				idx1 += len
+			}
+		} else {
+			idx1 += len
+		}
+		idx2 += len2
+	}
 	return true
 }
